b2630829d9f84066a35600791bcd4c63d78e2c01
paolaardon <ardonppa@gmail.com>
2019-09-08 22:00:48 +0100

    adding gallery

---
+++ js/new.js
@@ -0,0 +1,13 @@
+$('.image-link').magnificPopup({
+		type: 'image',
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+
+		image: {
+			verticalFit: true,
+		},
+
+    gallery: {
+      enabled: true
+    },
+	});
